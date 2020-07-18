e9cf4200a93b871181bd06b5f0731733bb40a2c0
Josh Kuiros <joshkuiros@gmail.com>
2020-07-18 12:48:59 -0700

    Fix overflow

--- src/components/PostListItem/styles.js
+++ src/components/PostListItem/styles.js
@@ -3,6 +3,9 @@ import styled from "styled-components";
 export const ListItem = styled.div`
   display: flex;
   margin-top: 0.5rem;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
 `;
 
 export const Date = styled.div`
