b0ee0c3b5100dc094db7290fb90c860ba71f9720
Josh Kuiros <joshkuiros@gmail.com>
2020-07-18 12:54:03 -0700

    Revert "Fix overflow"
    This reverts commit e9cf4200a93b871181bd06b5f0731733bb40a2c0.

--- src/components/PostListItem/styles.js
+++ src/components/PostListItem/styles.js
@@ -3,9 +3,6 @@ import styled from "styled-components";
 export const ListItem = styled.div`
   display: flex;
   margin-top: 0.5rem;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
 `;
 
 export const Date = styled.div`
